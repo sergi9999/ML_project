880a0608503e3729cc3d7f6565e797e9bed860dd
sergi9999 <113998343+sergi9999@users.noreply.github.com>
2022-09-20 16:10:09 +0200

    Update practical_session.py

--- practical_session.py
+++ practical_session.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("Practical_day_2_ML_APP")
-my_text=st.text("Hola que tal")
-my_button = st.button("Run mL computation")
+my_text=st.text("Do you want to be rich?")
+my_button = st.button("Click here to be rich")
 
 if my_button:
-    st.title("The model is running now...")
+    st.title("What a sucker :D")
 #st.markdown("")
