528dcb06dbca449d8d21ebc45a93c7e5651eeb71
sergi9999 <113998343+sergi9999@users.noreply.github.com>
2022-09-20 15:51:04 +0200

    Add files via upload

---
+++ practical_session.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title("Practical_day_2_ML_APP")
+my_text=st.text("Hola que tal")
+my_button = st.button("Run mL computation")
+
+if my_button:
+    st.title("The model is running now...")
+#st.markdown("")
