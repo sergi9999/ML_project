2dbeddd5ae4f3fb9bd92e2e14a55d1381365e33d
sergi9999 <113998343+sergi9999@users.noreply.github.com>
2022-09-20 15:53:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-echo "# streamlit-to-heroku-tutorial" >> README.md
+echo "# ML_project" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
 git branch -M master
-git remote add origin https://github.com/
+git remote add origin https://github.com/sergi9999/ML_project.git
 git push -u origin master
